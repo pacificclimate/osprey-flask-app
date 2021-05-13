2fd9b4b00117fd77b95a508ea7d13206e0bc2a16
Eric Yvorchuk <eyvorchuk@pcic-2005.pcic.uvic.ca>
2021-05-13 10:27:13 -0700

    Add docstring for route function

--- osprey_flask_app/routes.py
+++ osprey_flask_app/routes.py
@@ -11,10 +11,31 @@ data = Blueprint("data", __name__, url_prefix="/data")
 
 
 @data.route(
-    "/test",
+    "/",
     methods=["GET", "POST"],
 )
 def osprey_route():
+    """Provide route to get input parameters for full_rvic process.
+    Expected inputs (given in url)
+        1. case_id (str): Case ID for the RVIC process
+        2. grid_id (str): Routing domain grid shortname
+        3. run_startdate (str): Run start date (yyyy-mm-dd-hh). Only used for startup and drystart runs.
+        4. stop_date (str): Run stop date.
+        5. pour_points (path): Comma-separated file of outlets to route to [lons, lats]
+        6. uh_box (path): Defines the unit hydrograph to route flow to the edge of each grid cell.
+        7. routing (path): Routing inputs netCDF.
+        8. domain (path): CESM compliant domain file.
+        9. input_forcings (path): Land data netCDF forcings.
+        10. loglevel (str): Logging level (one of 'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'). Default is 'INFO'.
+        11. version (int): Return RVIC version string (1) or not (0). Default is 1.
+        12. np (int): Number of processors used to run job. Default is 1.
+        13. params_config_file (path): Path to input configuration file Parameters process.
+        14. params_config_dict (str): Dictionary containing input configuration for Parameters process (mutually exclusive with params_config_file).
+        15. convolve_config_file (path): Path to input configuration file Convolution process.
+        16. convolve_config_dict (str): Dictionary containing input configuration for Convolution process (mutually exclusive with convolve_config_file).
+
+    Returns output netCDF file after Convolution process.
+    """
     args = request.args
     version = True if args.get("version", default=1) == 1 else False
     arg_dict = {
@@ -35,7 +56,6 @@ def osprey_route():
         "convolve_config_file": args.get("convolve_config_file"),
         "convolve_config_dict": args.get("convolve_config_dict"),
     }
-    # outpath = arg_dict["domain"]  # Test getting input file
     outpath = run_full_rvic(arg_dict)
     outpath_url = requests.get(outpath)
     with NamedTemporaryFile(suffix=".nc", dir="/tmp") as outfile:
