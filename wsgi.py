aaca37a5a4bd9958a5fb4dab766a71884af2a15e
Eric Yvorchuk <eric.yvorchuk@gmail.com>
2021-05-28 09:12:37 -0700

    Use argparse to input host and port number

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
 from osprey_flask_app import create_app
+import argparse
 
+#parser = argparse.ArgumentParser(description="Initialize hostname and port number.")
+#parser.add_argument("--host", type=str, default="0.0.0.0", help="Host name for running app.")
+#parser.add_argument("--port", type=int, default=5000, help="Port number for running app.")
+#args = parser.parse_args()
 
 app = create_app()
 
