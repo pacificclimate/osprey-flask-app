c2b076cc18eb01cab9da997749a8804a5d1c45f0
eyvorchuk <eric.yvorchuk@gmail.com>
2021-08-31 15:35:04 -0700

    Split input file gathering into smaller functions

--- osprey_flask_app/utils.py
+++ osprey_flask_app/utils.py
@@ -6,6 +6,18 @@ import json
 from dateutil.parser import parse
 
 
+def get_base_urls():
+    """Get base THREDDS urls used for obtaining full input filepaths."""
+    url_prefix = "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds"
+    url_suffix = "datasets/storage/data/projects/hydrology/vic_gen2"
+    base_http_url = f"{url_prefix}/fileServer/{url_suffix}"
+    base_opendap_url = f"{url_prefix}/dodsC/{url_suffix}"
+    http_routing_url = f"{base_http_url}/input/routing"  # Contains unit hydrograph file for Parameters process
+    opendap_routing_url = f"{base_opendap_url}/input/routing"  # Contains input netCDF files for Parameters process
+    projections_url = f"{base_opendap_url}/output/projections_LE"  # Contains input netCDF files for Convolution process
+    return (http_routing_url, opendap_routing_url, projections_url)
+
+
 def find_nearest(domain, lon, lat):
     """Find indices of lon/lat coordinate in domain file that is closest to given pour point.
     Parameters
@@ -29,25 +41,68 @@ def find_nearest(domain, lon, lat):
     return (lon_index, lat_index)
 
 
+def check_region(
+    routing_url,
+    projections_url,
+    model_subdir,
+    region,
+    region_files,
+    lon,
+    lat,
+    prev_grid_id,
+):
+    """Check if lon/lat coordinate is completely inside this region. Return THREDDS filepaths if so.
+    Parameters
+        1. routing_url (str): THREDDS url for RVIC parameters netCDF files.
+        2. projections_url (str): THREDDS url for RVIC convolution input forcings files.
+        3. model_subdir (str): Subdirectory of climate model used to obtain input forcings.
+        4. region (str): Name of region.
+        5. region_files (dict): Input netCDF files for current region.
+        6. lon (float): longitude for pour point.
+        7. lat (float): latitude for pour point.
+        8. prev_grid_id (str): Routing domain grid shortname for previous pour point.
+    """
+    domain_file = region_files["domain"]
+    domain = (
+        f"{routing_url}/{region}/parameters/{domain_file}"  # CESM compliant domain file
+    )
+    (lon_index, lat_index) = find_nearest(domain, lon, lat)
+    if (lon_index, lat_index) == (-1, -1):
+        return (None, None, None, None)
+    frac = np.ma.getdata(
+        domain["frac"]
+    )  # Values are either masked (outside region), < 1 (partially in region), or 1 (completely in region)
+    pour_point_frac = frac[lat_index][lon_index]
+    if not np.ma.getmask(pour_point_frac) and pour_point_frac == 1:
+        if (
+            prev_grid_id != None and prev_grid_id != region.upper()
+        ):  # Only relevant for pour points after first one
+            raise ValueError("All pour points must be in the same region.")
+        else:
+            routing_file = region_files["routing"]
+            forcings_file = region_files["forcings"]
+            grid_id = f"{region.upper()}"  # Routing domain grid shortname
+            routing = f"{routing_url}/{region}/parameters/{routing_file}"  # Routing inputs netCDF
+            input_forcings = f"{projections_url}/{region}/{region.upper()}/{model_subdir}/{forcings_file}"  # Land data netCDF forcings
+            return (grid_id, routing, domain, input_forcings)
+
+    # Point is not in this region
+    return (None, None, None, None)
+
+
 def get_input_files(arg_dict):
     """Use lon/lat tuples to determine what input files to use for osprey.
     Parameters
         1. arg_dict (dict): dictionary to contain mappings to files
     """
-
-    url_prefix = "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds"
-    url_suffix = "datasets/storage/data/projects/hydrology/vic_gen2"
-    base_http_url = f"{url_prefix}/fileServer/{url_suffix}"
-    base_opendap_url = f"{url_prefix}/dodsC/{url_suffix}"
-    routing_url = f"{base_opendap_url}/input/routing"  # Contains input netCDF files for Parameters process
-    projections_url = f"{base_opendap_url}/output/projections_LE"  # Contains input netCDF files for Convolution process
+    (http_routing_url, opendap_routing_url, projections_url) = get_base_urls()
     model = arg_dict["model"]  # Climate model to use to get input forcings
     model_subdir = f"{model}/flux"
 
     new_arg_dict = dict(arg_dict)
     new_arg_dict[
         "uh_box"
-    ] = f"{base_http_url}/input/routing/uh/uhbox.csv"  # Unit hydrograph to route flow to the edge of each grid cell. Used for all RVIC runs
+    ] = f"{http_routing_url}/uh/uhbox.csv"  # Unit hydrograph to route flow to the edge of each grid cell. Used for all RVIC runs
     lons = new_arg_dict["lons"].split(",")
     lats = new_arg_dict["lats"].split(",")
     if len(lons) != len(lats):
@@ -55,41 +110,28 @@ def get_input_files(arg_dict):
 
     domains = json.load(open("domains.json"))
     nc_files = domains["nc_files"]
-    routing_file = None
+    prev_grid_id = None  # Used to check that all pour points are in the same domain
     for (lon, lat) in zip(lons, lats):
         found_region = False
         for region in nc_files.keys():
-            domain_file = nc_files[region]["domain"]
-            domain = netCDF4.Dataset(f"{routing_url}/{region}/parameters/{domain_file}")
-            (lon_index, lat_index) = find_nearest(domain, float(lon), float(lat))
-            if (lon_index, lat_index) == (-1, -1):
-                continue
-            frac = np.ma.getdata(
-                domain["frac"]
-            )  # Values are either masked (outside region), < 1 (partially in region), or 1 (completely in region)
-            pour_point_frac = frac[lat_index][lon_index]
-            if not np.ma.getmask(pour_point_frac) and pour_point_frac == 1:
-                if (
-                    routing_file != None and routing_file != nc_files[region]["routing"]
-                ):  # Only relevant for pour points after first one
-                    raise ValueError("All pour points must be in the same region.")
-                else:
-                    found_region = True
-                    routing_file = nc_files[region]["routing"]
-                    forcings_file = nc_files[region]["forcings"]
-                    new_arg_dict[
-                        "grid_id"
-                    ] = f"{region.upper()}"  # Routing domain grid shortname
-                    new_arg_dict[
-                        "routing"
-                    ] = f"{routing_url}/{region}/parameters/{routing_file}"  # Routing inputs netCDF
-                    new_arg_dict[
-                        "domain"
-                    ] = f"{routing_url}/{region}/parameters/{domain_file}"  # CESM compliant domain file
-                    new_arg_dict[
-                        "input_forcings"
-                    ] = f"{projections_url}/{region}/{region.upper()}/{model_subdir}/{forcings_file}"  # Land data netCDF forcings
-                    break
+            (grid_id, routing, domain, input_forcings) = check_region(
+                opendap_routing_url,
+                projections_url,
+                model_subdir,
+                region,
+                nc_files[region],
+                float(lon),
+                float(lat),
+                prev_grid_id,
+            )
+            if grid_id != None:
+                found_region = True
+                prev_grid_id = grid_id
+                new_arg_dict["grid_id"] = grid_id
+                new_arg_dict["routing"] = routing
+                new_arg_dict["domain"] = domain
+                new_arg_dict["input_forcings"] = input_forcings
+                break
         if not found_region:
             raise ValueError(
                 f"Pour point ({lon}, {lat}) not found in any of PCIC's modelled domains"
@@ -120,7 +162,6 @@ def create_pour_points(arg_dict):
     Parameters
         1. arg_dict (dict): dictionary containing coordinates and mapping to pour points file
     """
-
     lons = arg_dict["lons"].split(",")
     lats = arg_dict["lats"].split(",")
     attrs = {"lons": lons, "lats": lats}
@@ -140,7 +181,6 @@ def create_full_arg_dict(args):
     Parameters
         1. args (request.args): arguments given by url
     """
-
     # Optional url arguments
     opt_args = {
         "model": "ACCESS1-0_rcp45_r1i1p1",
@@ -168,14 +208,13 @@ def inputs_are_valid(arg_dict):
 
             1. run_startdate (str): Run start date. Only used for startup and drystart runs.
             2. stop_date (str): Run stop date.
-            3. model (str): Climate model to use to get input forcings.
-            4. pour points (str): Outlets to route to [lons, lats].
+            3. pour points (str): Outlets to route to [lons, lats].
+            4. model (str): Climate model to use to get input forcings.
             5. uh_box (path): Defines the unit hydrograph to route flow to the edge of each grid cell.
             6. routing (path): Routing inputs netCDF.
             7. domain (path): CESM compliant domain file.
             8. input_forcings (path): Land data netCDF forcings.
     """
-
     # Check start/stop dates
     parse(arg_dict["run_startdate"])
     parse(arg_dict["stop_date"])
