e90ea0f4430cc1a23ff901e61e6ebd4cf4850f06
eyvorchuk <eric.yvorchuk@gmail.com>
2021-08-31 09:54:08 -0700

    Remove unused util functions

--- osprey_flask_app/utils.py
+++ osprey_flask_app/utils.py
@@ -6,23 +6,6 @@ import json
 from dateutil.parser import parse
 
 
-def setup_logging(log_level):
-    formatter = logging.Formatter(
-        "%(asctime)s %(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S"
-    )
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logger = logging.getLogger("scripts")
-    logger.addHandler(handler)
-    logger.setLevel(getattr(logging, log_level))
-    return logger
-
-
-def get_filename_from_path(path):
-    """Given a path, return the filename on the end"""
-    return path.split("/")[-1]
-
-
 def find_nearest(domain, lon, lat):
     """Find indices of lon/lat coordinate in domain file that is closest to given pour point.
     Parameters
