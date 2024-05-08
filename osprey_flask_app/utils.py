0b5f5f9425551db02d2398df4c2bb1cb9499e2f9
Eric Yvorchuk <eric.yvorchuk@gmail.com>
2024-05-08 13:52:00 -0700

    Format with black

--- osprey_flask_app/utils.py
+++ osprey_flask_app/utils.py
@@ -9,7 +9,9 @@ from dateutil.parser import parse
 
 def get_base_urls():
     """Get base THREDDS urls used for obtaining full input filepaths."""
-    url_prefix = os.environ.get("THREDDS_URL", "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds")
+    url_prefix = os.environ.get(
+        "THREDDS_URL", "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds"
+    )
     url_suffix = "datasets/storage/data/projects/hydrology/vic_gen2"
     base_http_url = f"{url_prefix}/fileServer/{url_suffix}"
     base_opendap_url = f"{url_prefix}/dodsC/{url_suffix}"
